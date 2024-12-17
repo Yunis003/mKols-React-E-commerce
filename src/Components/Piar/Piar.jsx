c0505c24578ece2429e149c2307f4da7cbc615fb
Gulamov Yunis <139101778+Yunis003@users.noreply.github.com>
2024-12-17 23:59:52 +0400

    piar_design

--- src/Components/Piar/Piar.jsx
+++ src/Components/Piar/Piar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './piar.css';
 import decoraction from './1.svg';
+import hat from './hat.svg'
+import ball from './ball.svg'
+import sugarcane from './sugarcane.svg'
+import light from './light.png'
 const Piar = () => {
     return (
      <section className="piar">
         <img src={decoraction} alt="decor" />
-        <img src={decoraction} alt="decor" className='decor' />
-        <img src={decoraction} alt="decor" className='decor' />
-        <h1>Big sales around 26-29 December!</h1>
-        <img src={decoraction} alt="decor" className='decor' />
-        <img src={decoraction} alt="decor" className='decor' />
+        <img src={light} alt="light" className='decor1 lighter' />
+        
+      
+        <h1 className='txt_main'>Split the discounted price up to 24 months</h1>
+        <span className='between-time'>December 27 - December 29</span>
+
+        <img src={sugarcane} alt="sugarcane" className='sugarcane decor1' />
+        <img src={ball} alt="ball" className='ball decor2' />
+        <img src={hat} alt="hat" className='hat decor3' />
         <img src={decoraction} alt="decor" />
 
         </section>   
