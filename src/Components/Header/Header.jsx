9b4421cc4e8a2d0848d2314c6f4e4ad44e4e7942
Gulamov Yunis <139101778+Yunis003@users.noreply.github.com>
2024-12-26 15:24:49 +0400

    fixed-bugs

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './header.css';
-import Logo from './logo.svg';
+import Logo from './siteLogo.svg';
 import Main from '../MainAll/Main';
 import Contact from '../Contact/Contact';
 import About from '../About/About';
